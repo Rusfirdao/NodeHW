59eb01062b0e697e486a1615e742d69f9d94e49c
Rusfirdao <fiirdaoo@gmail.com>
2020-11-15 22:23:11 +0700

    Monitoring with winston logger

--- converter.js
+++ converter.js
@@ -1,7 +1,11 @@
+const { loggers } = require("winston");
+
 function converter(currency) {
     if (currency < 0) {
         return currency;
-    } else if (currency == currency.toFixed(1) || currency == currency.toFixed(2)) {
+    }
+    
+     else if (currency == currency.toFixed(1) || currency == currency.toFixed(2)) {
         return "$" + (currency / 31.04).toFixed(2);
     }
 
